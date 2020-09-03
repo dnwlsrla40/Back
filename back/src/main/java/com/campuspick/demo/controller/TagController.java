511544cb7a7cf3f3c97538f9a21a78146873fd87
YYJ <hidua1124@naver.com>
2020-09-04 00:06:16 +0900

    [변경] 태그 목록 페이지 페이징 및 인기순/이름순 구현
    - 태그 40개씩 가져오도록 페이징
    - 인기순(trending)/이름순(alphabetical) 구현

--- back/src/main/java/com/campuspick/demo/controller/TagController.java
+++ back/src/main/java/com/campuspick/demo/controller/TagController.java
@@ -6,6 +6,7 @@ import com.campuspick.demo.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +18,8 @@ public class TagController {
     private TagService tagService;
 
     @GetMapping("/velog.io/tags")
-    public List<TagListResponseDto> listTags() {
-        return tagService.getTagList();
+    public List<TagListResponseDto> listTags(@RequestParam(defaultValue = "trending") String sort, int page) {
+        return tagService.getTagList(sort, page);
     }
 
     @GetMapping("/velog.io/tags/{tagName}")
--- back/src/main/java/com/campuspick/demo/domain/repository/PostTagRepository.java
+++ back/src/main/java/com/campuspick/demo/domain/repository/PostTagRepository.java
@@ -1,12 +1,8 @@
 package com.campuspick.demo.domain.repository;
 
-import com.campuspick.demo.domain.entity.Post;
 import com.campuspick.demo.domain.entity.PostTag;
-import com.campuspick.demo.domain.entity.Tag;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
