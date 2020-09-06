8b2ea9896cfcf710402bd25540fe5b7c3cb23055
YYJ <hidua1124@naver.com>
2020-09-07 02:12:37 +0900

    [변경] URI 구성에 @RequestMapping("/velog.io") 사용

--- back/src/main/java/com/campuspick/demo/controller/TagController.java
+++ back/src/main/java/com/campuspick/demo/controller/TagController.java
@@ -4,25 +4,23 @@ import com.campuspick.demo.dto.TagDetailResponseDto;
 import com.campuspick.demo.dto.TagListResponseDto;
 import com.campuspick.demo.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/velog.io")
 @RestController
 public class TagController {
 
     @Autowired
     private TagService tagService;
 
-    @GetMapping("/velog.io/tags")
+    @GetMapping("/tags")
     public List<TagListResponseDto> listTags(@RequestParam(defaultValue = "trending") String sort, int page) {
         return tagService.getTagList(sort, page);
     }
 
-    @GetMapping("/velog.io/tags/{tagName}")
+    @GetMapping("/tags/{tagName}")
     public TagDetailResponseDto getTagDetail(@PathVariable String tagName) {
         return tagService.getTagDetail(tagName);
     }
--- back/src/main/java/com/campuspick/demo/controller/UserController.java
+++ back/src/main/java/com/campuspick/demo/controller/UserController.java
@@ -9,59 +9,60 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Blob;
 import java.util.UUID;
 
+@RequestMapping("/velog.io")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/velog.io/setting")
+    @GetMapping("/setting")
     public User getUserSetting(String id){
         return userService.getUserInfo(UUID.fromString(id));
     }
 
-    @PutMapping("/velog.io/setting/thumbnail")
+    @PutMapping("/setting/thumbnail")
     public User updateUserThumbnail(String id, @RequestBody Blob thumbnail){
         return userService.updateUserThumbnail(UUID.fromString(id), thumbnail);
     }
 
-    @DeleteMapping("/velog.io/setting/thumbnail")
+    @DeleteMapping("/setting/thumbnail")
     public User deleteUserThumbnail(String id){
         return userService.deleteUserThumbnail(UUID.fromString(id));
     }
 
-    @PutMapping("/velog.io/setting/shortbio")
+    @PutMapping("/setting/shortbio")
     public User updateUserShortBio(String id, @RequestBody String shortBio){
         return userService.updateUserShortBio(UUID.fromString(id), shortBio);
     }
 
-    @PutMapping("/velog.io/setting/velogname")
+    @PutMapping("/setting/velogname")
     public User updateUserVelogName(String id, @RequestBody String velogName){
         return userService.updateUserVelogName(UUID.fromString(id), velogName);
     }
 
-    @PutMapping("/velog.io/setting/profilelinks")
+    @PutMapping("/setting/profilelinks")
     public User updateUserProfileLinks(String id, @RequestBody String profileLinks){
         return userService.updateUserProfileLinks(UUID.fromString(id), profileLinks);
     }
 
-    @DeleteMapping("/velog.io/setting")
+    @DeleteMapping("/setting")
     public String deleteUser(String id){
         userService.deleteUser(UUID.fromString(id));
         return id;
     }
 
-    @GetMapping("/velog.io/@{username}")
+    @GetMapping("/@{username}")
     public UserVelogResponseDto getUserVelog(@PathVariable String username, String tagname){
         return userService.getUserVelog(username, tagname);
     }
 
-    @GetMapping("/velog.io/@{username}/about")
+    @GetMapping("/@{username}/about")
     public User getUserDetail(@PathVariable String username){
         return userService.getUserInfo(username);
     }
 
-    @PutMapping("/velog.io/@{username}/about")
+    @PutMapping("/@{username}/about")
     public User updateUserAbout(@PathVariable String username, @RequestBody String about){
         return userService.updateUserAbout(username, about);
     }
